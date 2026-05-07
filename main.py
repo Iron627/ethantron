0de6863aac6eb2910b12b0599126bea4d9cbcaa8
Iron6271 <irononyt69@gmail.com>
2026-05-08 01:02:15 +0530

    draws a board now!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 pygame.init()
-SCREEN_HEIGHT,SCREEN_WIDTH = 854,480
+SCREEN_HEIGHT,SCREEN_WIDTH = 1024,1024
 screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
 running = True
 clock = pygame.time.Clock()
@@ -27,11 +27,23 @@ class Board:
             [1, 1, 1, 1, 1, 1, 1, 1],
             [4, 2, 3, 5, 6, 3, 2, 4],
         ]
+    def draw(self):
+        cell_width = SCREEN_WIDTH // 8
+        cell_height = SCREEN_HEIGHT // 8
+        for row_no, row in enumerate(self.board):
+            for col_no, _ in enumerate(row):
+                color = "white" if (row_no + col_no) % 2 == 0 else "black"
+                rect = (row_no * cell_width, col_no * cell_height, cell_width, cell_height)
+                pygame.draw.rect(self.screen, color, rect)
 
+
+
+
+game_board = Board(screen)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill("white")
+    game_board.draw()
     pygame.display.flip()
     clock.tick(240)
